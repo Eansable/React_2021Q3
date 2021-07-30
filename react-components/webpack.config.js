5184256153891221475136aa30158133c6bf7e93
Andrei Tarasevich <eansable@yandex.by>
2021-07-31 00:49:44 +0300

    init: init project and setting webpack

---
+++ react-components/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const esLintPlugin = (isDev) => isDev ? [] : [ new EslintPlugin({ extensions: ['ts', 'js', 'tsx', 'jsx'] }) ]
+
+module.exports = ({dev}) => ({
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[hash][ext]'
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+},
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg|webp)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    ...esLintPlugin(dev)
+  ]
+}
+)
